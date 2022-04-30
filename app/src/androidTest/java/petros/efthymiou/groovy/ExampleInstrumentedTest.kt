591c5fb7b01ff9b851289ddc3e018411ec46d6df
khindi <kaverip.hindi@amadeus.com>
2022-04-30 18:30:38 +0530

    Implemented first test case

--- app/src/androidTest/java/petros/efthymiou/groovy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/petros/efthymiou/groovy/ExampleInstrumentedTest.kt
@@ -2,11 +2,14 @@ package petros.efthymiou.groovy
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +18,13 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+
+    val activityTestRule = ActivityTestRule(MainActivity::class.java)
+            @Rule get
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("petros.efthymiou.groovy", appContext.packageName)
+    fun testPlaylistsDisplayed(){
+        assertDisplayed("Playlists")
     }
 }
